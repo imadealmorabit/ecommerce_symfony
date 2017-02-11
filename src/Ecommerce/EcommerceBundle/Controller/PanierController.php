3894fd3694c1369bc7669248c767d4b2775baf55
imadealmorabit <imadealmorait@gmail.com>
2017-02-11 00:53:26 +0000

    implementation de l'action ajouterAction le controlleur panierController

--- src/Ecommerce/EcommerceBundle/Controller/PanierController.php
+++ src/Ecommerce/EcommerceBundle/Controller/PanierController.php
@@ -3,9 +3,39 @@
 namespace Ecommerce\EcommerceBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class PanierController extends Controller
 {
+    public function ajouterAction($id, Request $request)
+    {
+        $session = $request->getSession();
+        
+        if(!$session->has('panier')) $session->set('panier', array());
+        
+        $panier = $session->get('panier');
+        
+        if(array_key_exists($id, $panier)){
+            
+            if($request->query->get('qte') != null) $panier['$id'] = $request->query->get('qte');          
+        }
+        else
+        {
+            if($request->query->get('qte') != null) $panier['$id'] = $request->query->get('qte');
+            else
+                $panier['$id'] = 1;
+        }
+        $session->set('panier', $panier);
+        
+        return $this->redirectToRoute('panier');
+    }
+    
+    public function supprimerAction($id)
+    {
+        return $this->render('EcommerceBundle:Default/panier/layout:panier.html.twig');
+    }
+    
+    
     public function panierAction()
     {
         return $this->render('EcommerceBundle:Default/panier/layout:panier.html.twig');
