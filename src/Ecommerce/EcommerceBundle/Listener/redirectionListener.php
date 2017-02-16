426c0207bc88b35e0b099ff766921aef43a3b144
imadealmorabit <imadealmorait@gmail.com>
2017-02-16 00:50:33 +0000

    ajout du listener pour la validation

---
+++ src/Ecommerce/EcommerceBundle/Listener/redirectionListener.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Ecommerce\EcommerceBundle\Listener;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+class redirectionListener {
+
+    public function __construct(ContainerInterface $container, Session $session) {
+        $this->session = $session;
+        $this->router = $container->get('router');
+        $this->securityContext = $container->get('security.token_storage');
+    }
+
+    public function onKernelRequest(GetResponseEvent $event) {
+        $route = $event->getRequest()->attributes->get('_route');
+
+        if ($route == 'livraison' || $route == 'validation') {
+            if ($this->session->has('panier')) {
+                if (count($this->session->get('panier')) == 0)
+                    $event->setResponse($this->redirectToRoute('panier'));
+            }
+
+
+            if (!is_object($this->securityContext->getToken()->getUser())) {
+                $this->session->getFlashBag()->add('notification', 'Vous devez vous identifier');
+                $event->setResponse($this->redirectToRoute('fos_user_security_login'));
+            }
+        }
+    }
+
+}
